2ec8773fd3fbc446d4803fc0bb76011fa123f824
melbosse <127343358+melbosse@users.noreply.github.com>
2023-04-19 15:32:30 -0400

    Ajout de l'introduction

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner monObj = new Scanner(System.in);
+        System.out.println("Bienvenue au jeu de Tic Tac Toe !");
+        System.out.println("Entrez votre prénom :");
+
+        String prenom = monObj.nextLine();
+        System.out.println("Bienvenue " + prenom + "!");
+
+        System.out.println("La partie va bientôt commencer.");
     }
 }
